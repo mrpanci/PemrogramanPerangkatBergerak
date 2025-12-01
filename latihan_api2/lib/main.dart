226a5334c39d0787c9958713ba49a32e1e2bd369
firdausfachry <firdausfachry23@gmail.com>
2025-12-01 11:24:17 +0700

    Pertemuan 10 dan 11
    Latihan membuat login session dan integrasi dengan database

--- latihan_api2/lib/main.dart
+++ latihan_api2/lib/main.dart
@@ -3,12 +3,17 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLogged = prefs.getString("username") != null;
+
+  runApp(MyApp(isLogged: isLogged));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final bool isLogged;
+  const MyApp({super.key, required this.isLogged});
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +21,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'latihan_api2',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const LoginPage(),
+      home: isLogged ? const UserListPage() : const LoginPage(),
     );
   }
 }
 
-//Login Page
+// Login Page
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -41,55 +46,51 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login")),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextField(
-              controller: usernameC,
-              decoration: const InputDecoration(
-                labelText: "Username",
-                border: OutlineInputBorder(),
+      body: Center(
+        child: Container(
+          width: 350,
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Login", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 30),
+              TextField(
+                controller: usernameC,
+                decoration: const InputDecoration(labelText: "Username", border: OutlineInputBorder()),
               ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: passwordC,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(),
+              const SizedBox(height: 20),
+              TextField(
+                controller: passwordC,
+                obscureText: true,
+                decoration: const InputDecoration(labelText: "Password", border: OutlineInputBorder()),
               ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                if (usernameC.text.isEmpty || passwordC.text.isEmpty) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text("Username dan Password tidak boleh kosong"),
-                    ),
-                  );
-                  return;
-                }
+              const SizedBox(height: 25),
+              ElevatedButton(
+                onPressed: () async {
+                  if (usernameC.text.isEmpty || passwordC.text.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Username dan Password tidak boleh kosong")),
+                    );
+                    return;
+                  }
 
-                await saveLogin();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (_) => const UserListPage()),
-                );
-              },
-              child: const Text("Login"),
-            )
-          ],
+                  await saveLogin();
+                  // ignore: use_build_context_synchronously
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const UserListPage()));
+                },
+                style: ElevatedButton.styleFrom(minimumSize: const Size(double.infinity, 45)),
+                child: const Text("Login"),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-// User List
+// User List Page
 class UserListPage extends StatefulWidget {
   const UserListPage({super.key});
 
@@ -100,14 +101,11 @@ class UserListPage extends StatefulWidget {
 class _UserListPageState extends State<UserListPage> {
   List users = [];
   bool isLoading = false;
+  String username = "";
 
   Future<void> fetchUsers() async {
     setState(() => isLoading = true);
-
-    final response = await http.get(
-      Uri.parse("https://jsonplaceholder.typicode.com/users"),
-    );
-
+    final response = await http.get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
     if (response.statusCode == 200) {
       setState(() {
         users = json.decode(response.body);
@@ -121,22 +119,33 @@ class _UserListPageState extends State<UserListPage> {
     }
   }
 
+  Future<void> loadSession() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() => username = prefs.getString("username") ?? "");
+  }
+
+  Future<void> logout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const LoginPage()));
+  }
+
   @override
   void initState() {
     super.initState();
     fetchUsers();
+    loadSession();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Daftar Pengguna"),
+        title: Text("Halo, $username 👋"),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            onPressed: fetchUsers,
-          ),
+          IconButton(onPressed: fetchUsers, icon: const Icon(Icons.refresh)),
+          IconButton(onPressed: logout, icon: const Icon(Icons.logout)),
         ],
       ),
       body: isLoading
@@ -146,15 +155,10 @@ class _UserListPageState extends State<UserListPage> {
               itemBuilder: (context, index) {
                 final u = users[index];
                 return Card(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 5,
-                    horizontal: 10,
-                  ),
                   child: ListTile(
                     title: Text(u['name']),
-                    subtitle: Text(
-                      "${u['email']} • ${u['address']['city']}",
-                    ),
+                    subtitle: Text("${u['email']} • ${u['address']['city']}"),
+                    trailing: const Icon(Icons.chevron_right),
                   ),
                 );
               },
